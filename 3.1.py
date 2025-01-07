c3f8930eaeae000b77e45a879fe10c070067c6d0
@5yanis <yr2980@gmail.com>
2025-01-07 08:54:25 +0100

    oefening 3 nummer 1

--- 3.1.py
+++ 3.1.py
@@ -1,17 +1,27 @@
-prijs_maaltijd = 20 
- 
- 
-def prijs_bepalen():
+#het gegeven
+prijs_maaltijd =  20
+
+#bepalen van de korting
+
+def prijs_korting():
+
     if prijs_maaltijd > 15: 
-        korting = 0.1 
-        return korting
+
+        korting = 0.1
     else: 
 
         korting = 0
-        return korting 
-prijs_bepalen()
-totale_prijs = prijs_maaltijd - (prijs_maaltijd * korting) 
 
-print(f"Totale prijs: €{totale_prijs}") 
+    return korting
+prijs_korting()
 
-print("Bon: Bedankt voor uw bestelling!") 
+#De totale prijs bepalen
+
+def totale(korting):
+    totale_prijs=prijs_maaltijd-(prijs_maaltijd*korting)
+    print(f"Totale prijs: €{totale_prijs}") 
+    print("Bon: Bedankt voor uw bestelling!") 
+
+
+
+totale(prijs_korting())
