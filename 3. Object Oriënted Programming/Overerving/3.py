eb0ba81e3da4516a84b0306387c369602dda5334
@5yanis <yr2980@gmail.com>
2025-01-21 15:45:13 +0100

    oefening 3 overerving

--- 3. Object Oriënted Programming/Overerving/3.py
+++ 3. Object Oriënted Programming/Overerving/3.py
@@ -1,1 +1,24 @@
-class persoon
+class persoon:
+    def __init__(self, naam, medewerker=False):
+        self.naam=naam
+        self.medewerker=medewerker
+    def krijg_naam(self):
+        return "naam: {}".format(self.naam)
+    def isMedewerker(self):
+        return "Klasse: {}".format(self.medewerker)
+
+
+
+
+class medewerker(persoon):
+    def krijg_naam(self):
+        return "naam: {}".format(self.naam)
+    def isMedewerker(self):
+        self.medewerker=True
+        return "Klasse: {}".format(self.medewerker)
+
+
+persoon1=persoon("Jan")
+medewerker1=medewerker("Ben")
+print(persoon1.krijg_naam(), persoon1.isMedewerker())
+print(medewerker1.krijg_naam(), medewerker1.isMedewerker())
