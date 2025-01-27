ef0d5848ee98ce666793601b71b34d4f7a07042c
@5yanis <yr2980@gmail.com>
2025-01-27 13:49:13 +0100

    oefening 4 overerving

---
+++ 3. Object Oriënted Programming/Overerving/4.py
@@ -0,0 +1,21 @@
+class gegevens:
+    def __init__(self, naam):
+        self.naam=naam
+
+    def krijgnaam(self):
+        return "{} is een kleinkind van".format(self.naam)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+lana=gegevens("lana")
